32be47a378b981b984dfda970cd53a7102fe1ca3
Iurii Kozuliak <i.kozuliak@clearcode.cc>
2013-06-06 20:32:22 +0200

    Angular update, routing, main-view lot of improvements

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('homepageApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl:'views/main.html',
+                controller:'MainCtrl'
+            })
+            .when('/labs', {
+                templateUrl:'views/labs.html',
+                controller:'MainCtrl'
+            });
+    });
