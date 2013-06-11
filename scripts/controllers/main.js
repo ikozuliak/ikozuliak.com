6926092d1a38e34b031a9a071834a329513b77cc
Iurii Kozuliak <i.kozuliak@gmail.com>
2013-06-11 13:45:04 +0200

    Sunburst transitions, text arc

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -2,6 +2,7 @@
 
 angular.module('homepageApp')
     .controller('MainCtrl', function ($scope, $location) {
+
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
@@ -14,27 +15,39 @@ angular.module('homepageApp')
         };
 
 
-        var w = 720,
-            h = 720,
-            r = 360,
+        var w = 820,
+            h = 820,
+            r = 410,
+            angle = 0,
             color = d3.scale.category10();
 
 
         var partition = d3.layout.partition()
             .size([2 * Math.PI, r])
-            .value(function(d) { return d.size; });
+            .value(function (d) {
+                return d.size;
+            });
 
         var arc = d3.svg.arc()
-            .startAngle(function(d) { return d.x -.01; })
-            .endAngle(function(d) { return d.x + d.dx; })
-            .innerRadius(function(d) { return d.y; })
-            .outerRadius(function(d) { return d.y + d.dy + 1; });
+            .startAngle(function (d) {
+                return d.x - .01;
+            })
+            .endAngle(function (d) {
+                return d.x + d.dx;
+            })
+            .innerRadius(function (d) {
+                return d.y;
+            })
+            .outerRadius(function (d) {
+                return d.y + d.dy + 1;
+            });
 
         var svg = d3.select("#sunburst").append("svg:svg")
             .attr("width", w)
             .attr("height", h)
             .append("svg:g")
-            .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")");
+            .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")")
+            .on("mouseenter", rotate)
 
         var group = svg.data([getData()]).selectAll("path")
             .data(partition.nodes)
@@ -43,49 +56,58 @@ angular.module('homepageApp')
 //path variable is required by magnify function
         var path = group.append("svg:path")
             .attr("d", arc)
-            .style("fill", function(d) {return d.color || "#bcc5c9" })
-            .on("click", magnify)
+            .attr("id", function (d) {
+                return d.name
+            })
+            .style("fill", function (d) {
+                return d.color || "#bcc5c9"
+            })
+            .on("mouseenter", magnify)
             .each(stash);
 
         var text = group.append("text");
-        text.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-            .attr("dy", ".35em")
-            .style("text-anchor", "middle")
-            .text(function(d) { return d.name });
 
-
-
-
-        var arc = d3.svg.arc()
-            .innerRadius(150)
-            .outerRadius(180)
-            .startAngle(0)
-            .endAngle(-3.14/2)
-
-        var path = svg.append("path")
-            .attr("d", arc)
-            .attr("id", "path1")
-            .attr("transform", "translate(200,200)")
-            .attr("fill","#ccf")
-
-
-        var text = svg.append("text")
-            .attr("x", 0)
-            .attr("dy", 10);
+        text.attr("transform", function (d) {
+//            return "translate(" + arc.centroid(d) + ")";
+        })
+            .attr("dy", 52)
+            .attr("dx", 10)
+            .attr("text-anchor", "middle")
+            .attr("class", function (d) {
+                return d.class || d.name
+            })
 
         text.append("textPath")
-            .attr("stroke","black")
-            .attr("xlink:href","#path1")
-            .text("abc");
+            .attr("xlink:href", function (d) {
+                return "#" + d.name
+            })
+            .text(function (d) {
+//                return d.name
+            })
+            .attr("method", "align")
+            .attr("spacing", "exact")
+            .attr("startOffset", function (d) {
+                return d.offset || "21%"
+            })
 
+
+        function rotate(node) {
+            angle += 90;
+            svg
+                .transition()
+                .duration(1500)
+                .ease("back")
+                .attr("transform", "translate(" + w / 2 + "," + h / 2 + "), rotate( " + angle + ")")
+        }
 
 // Distort the specified node to 80% of its parent.
         function magnify(node) {
+
             if (parent = node.parent) {
                 var parent,
                     x = parent.x,
-                    k = .7;
-                parent.children.forEach(function(sibling) {
+                    k =  Math.floor(Math.random() * (9 - 4) + 4)/10; ;
+                parent.children.forEach(function (sibling) {
                     x += reposition(sibling, x, sibling === node
                         ? parent.dx * k / node.value
                         : parent.dx * (1 - k) / (parent.value - node.value));
@@ -97,7 +119,9 @@ angular.module('homepageApp')
             svg.selectAll("text")
                 .transition()
                 .duration(750)
-                .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+                .attr("transform", function (d) {
+//                    return "translate(" + arc.centroid(d) + ")";
+                })
 
 
             path.transition()
@@ -123,8 +147,8 @@ angular.module('homepageApp')
 
 // Interpolate the arcs in data space.
         function arcTween(a) {
-            var i = d3.interpolate({x: a.x0, dx: a.dx0}, a);
-            return function(t) {
+            var i = d3.interpolate({x:a.x0, dx:a.dx0}, a);
+            return function (t) {
                 var b = i(t);
                 a.x0 = b.x;
                 a.dx0 = b.dx;
@@ -134,46 +158,139 @@ angular.module('homepageApp')
 
         function getData() {
             return {
-                "name": "HTML5",
-                "color": "#1b2430",
-                "children": [
+                "name":" ",
+                "color":"#1b2430",
+                "children":[
                     {
-                        "name": "JavaScript",
-                        "color": "#fe4400",
-                        "children": [
+                        "name":"J a v a S c r i p t",
+                        "offset":"25%",
+                        "class":"javascript",
+                        "color":"#606d7a",
+                        "children":[
                             {
-                                "name": "child11",
-                                "children": [
-                                    {"name": "child111", "size": 60}
-                                ]
-                            }
-                        ]
-                    },
-                    {
-                        "name": "CASCADING STYLESHEETS",
-                        "color": "#ffffff",
-                        "children": [
-                            {
-                                "name": "",
-                                "color": "#ecf1f4",
-                                "children": [
-                                    {"name": "", "size": 20}
-                                ]
-                            },
-                            {
-                                "name": "Preprocessing",
-                                "color": "#ecf1f4",
+                                "name":"M V V C",
+                                "class":"mvvc",
+                                "color":"#fff",
                                 "children":[
-                                    {"name": "Less", "size": 7},
-                                    {"name": "Sass", "size": 7},
-                                    {"name": "Stylus", "size": 6}
+                                    {
+                                        "name":"A n g u l a r",
+                                        "class":"angular",
+                                        "offset":"15%",
+                                        "children":[
+                                            {
+                                                "name":"",
+                                                "color":"#f4f7f8",
+                                                "size":10
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "name":"B a c k b o n e",
+                                        "offset":"15%",
+                                        "children":[
+                                            {
+                                                "name":"",
+                                                "color":"#f4f7f8",
+                                                "size":10
+                                            }
+                                        ]
+                                    }
                                 ]
                             },
                             {
-                                "name": "CSS3",
-                                "color": "#1b2430",
-                                "children": [
-                                    {"name": "Animations", "size": 20}
+                                "name":"C o f f e s c r i p t",
+                                "color":"#fff",
+                                "children":[
+                                    {
+                                        "name":"",
+                                        "children":[
+                                            {
+                                                "name":"",
+                                                "color":"#f4f7f8",
+                                                "size":20
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                            {
+                                "name":"S e r v e r s i d e",
+                                "color":"#fff",
+                                "children":[
+                                    {
+                                        "name":"Node.js",
+                                        "children":[
+                                            {
+                                                "name":"",
+                                                "color":"#f4f7f8",
+                                                "size":20
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "name":"C  S  S",
+                        "class":"css",
+                        "offset":"25%",
+                        "color":"#ffffff",
+                        "children":[
+
+                            {
+                                "name":"P r e p r o c e s s i n g",
+                                "class":"preprocessing",
+                                "color":"#ecf1f4",
+                                "children":[
+                                    {
+                                        "name":"L E S S",
+                                        "children":[
+                                            {
+                                                "name":"",
+                                                "color":"#f4f7f8",
+                                                "size":14
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "name":"S a s s",
+                                        "children":[
+                                            {
+                                                "name":"",
+                                                "color":"#f4f7f8",
+                                                "size":13
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "name":"Stylus",
+                                        "children":[
+                                            {
+                                                "name":"",
+                                                "color":"#f4f7f8",
+                                                "size":13
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                            {
+                                "name":"C S S 3",
+                                "offset":"18%",
+                                "class":"css3",
+                                "color":"#1b2430",
+                                "children":[
+                                    {
+                                        "name":"Animations",
+                                        "children":[
+                                            {
+                                                "name":"",
+                                                "color":"#f4f7f8",
+                                                "size":20
+                                            }
+                                        ]
+                                    }
                                 ]
                             }
 
@@ -181,14 +298,13 @@ angular.module('homepageApp')
                     },
 
                     {
-                        "name": "html5",
-                        "color": "#606d7a",
-                        "children": [
+                        "name":"H T M L 5",
+                        "class":"html5",
+                        "color":"#fe4400",
+                        "children":[
                             {
-                                "name": "child11",
-                                "children": [
-                                    {"name": "child111", "size": 60}
-                                ]
+                                "name":"child11",
+                                "size":60
                             }
                         ]
                     }
