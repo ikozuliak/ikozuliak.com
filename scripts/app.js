bdd22f572278eb6793ef20e8fbe05a6e2bcf8c06
Iurii Kozuliak <i.kozuliak@gmail.com>
2013-06-19 16:12:59 +0200

    Layput update/ sunburst update

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,9 @@
 
 angular.module('homepageApp', [])
     .config(function ($routeProvider, $locationProvider) {
+
+//        $locationProvider.html5Mode(true);
+
         $routeProvider
             .when('/', {
                 templateUrl:'views/main.html',
